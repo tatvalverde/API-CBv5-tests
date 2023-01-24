102f9d053ef81876916fc73a6e168b166e055f87
Tatiana Naumova <tatiananaumova5@gmail.com>
2023-01-24 17:28:05 -0500

    changes in auth.spec.js

---
+++ tests/autn.spec.js
@@ -0,0 +1,23 @@
+import { expect } from 'chai'
+import {login} from '../helpers/general-helpers';
+
+describe('Authentication positive', () => {
+  describe('Login with valid credentials', () => {
+    let res
+    before(async () => {
+      res = await login(process.env.EMAIL, process.env.PASSWORD)
+    })
+
+    it('Login with valid credentials', async () => {
+      expect(res.statusCode).to.eq(200)
+    })
+
+    it('Response has a correct message', async () => {
+      expect(res.body.message).to.eq('Auth success')
+    })
+
+    it('Response has a token', async () => {
+      expect(res.body.payload.token).to.be.a('string')
+    })
+  })
+})
