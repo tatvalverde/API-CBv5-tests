fcdac8b75324996bee498f3f684b088946461f70
Tatiana Naumova <tatiananaumova5@gmail.com>
2023-01-24 17:30:19 -0500

    add tests in auth.spec.js

--- tests/autn.spec.js
+++ tests/autn.spec.js
@@ -20,4 +20,29 @@ describe('Authentication positive', () => {
       expect(res.body.payload.token).to.be.a('string')
     })
   })
+  describe('Authentication negative', () => {
+    describe('login with invalid credentials', () => {
+      let res
+
+      it('Login with invalid email', async () => {
+        res = await login('invalid@gmail.com', process.env.PASSWORD)
+        expect(res.statusCode).to.eq(400)
+      })
+
+      it('Login with invalid password', async () => {
+        res = await login(process.env.EMAIL, '123')
+        expect(res.statusCode).to.eq(400)
+      })
+
+      it('Login with empty fields', async () => {
+        res = await login('', '')
+        expect(res.statusCode).to.eq(400)
+      })
+
+      it('Response body contains error message', async () => {
+        res = await login('invalid@gmail.com', '123')
+        expect(res.body.message).to.eq('Auth failed')
+      })
+    })
+  })
 })
