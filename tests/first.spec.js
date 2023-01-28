868864d4013b1602ce3d62d9c538a4fbdfa053d0
Tatiana Naumova <tatiananaumova5@gmail.com>
2023-01-28 18:49:23 -0500

    Added tests in first.spec.js

--- tests/first.spec.js
+++ tests/first.spec.js
@@ -5,3 +5,26 @@ describe('example', () =>{
     expect(5+5).to.eq(10)
   })
 })
+describe('Operations with numbers', function () {
+  const a = 5
+  const b = 7
+
+  it('Addition works properly', function () {
+    expect(a + b).to.eq(12)
+  })
+  it('Subsraction works properly', function () {
+    expect(a - b).to.eq(-2)
+  })
+})
+
+describe('Operation with numbers', function () {
+  const a = 8
+  const b = 4
+
+  it('Multiplication works properly', function () {
+    expect(a * b).to.eq(32)
+  })
+  it('Division works properly', function () {
+    expect(a / b).to.eq(2)
+  })
+})
