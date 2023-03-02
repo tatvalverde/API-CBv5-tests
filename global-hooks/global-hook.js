92036991556fffd5d1313c5aea7319c8e3bdc537
Tatiana Naumova <tatiananaumova5@gmail.com>
2023-03-02 18:34:01 -0500

    delete hook

--- global-hooks/global-hook.js
+++
@@ -1,9 +0,0 @@
-import supertest from 'supertest' // file will be executed
-import 'dotenv/config'
-
-before(async ()=> {
-  let res = await supertest(process.env.BASE_URL)
-    .post('/v5/user/login')
-    .send({ email: process.env.EMAIL, password: process.env.PASSWORD })
-  process.env['TOKEN'] = res.body.payload.token
-})
